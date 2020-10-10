599b6eb7a18e77c62f633c495eacdd91fd6fb744
yeongbba <yeongmolee2@gmail.com>
2020-10-10 22:06:58 +0900

    useConfirm & usePrevent

---
+++ usePrevent/useprevent.js
@@ -0,0 +1,11 @@
+export const usePreventLeave = () => {
+  const listner = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listner);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listner);
+
+  return { enablePrevent, disablePrevent };
+};
---
+++ useTabs/usetabs.js
@@ -0,0 +1,11 @@
+export const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
